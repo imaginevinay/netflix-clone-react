031202c6387f3de80b9282d7a656a800469078a8
unknown <vinay@ironsystems.com>
2020-10-28 15:30:15 +0530

    Updates

---
+++ precache-manifest.b633c209f2e48e17f8fc77eafdad1e99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3f9e10e628284bc226f9d59841b997b",
+    "url": "/netflix-clone-react/index.html"
+  },
+  {
+    "revision": "7211271c9fcb16a3e756",
+    "url": "/netflix-clone-react/static/css/main.2857d2c2.chunk.css"
+  },
+  {
+    "revision": "c81c07e80247d1446140",
+    "url": "/netflix-clone-react/static/js/2.9d8d971a.chunk.js"
+  },
+  {
+    "revision": "5268dce82a7d7b285841c98f617d5abc",
+    "url": "/netflix-clone-react/static/js/2.9d8d971a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7211271c9fcb16a3e756",
+    "url": "/netflix-clone-react/static/js/main.073cd271.chunk.js"
+  },
+  {
+    "revision": "7ca54db292dc42a85f73",
+    "url": "/netflix-clone-react/static/js/runtime-main.bd7a6643.js"
+  }
+]);
